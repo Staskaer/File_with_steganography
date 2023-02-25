a65e2a5597f1aee03300a587aaea403a4a6cd0a5
ppxxxg22 <liujiaxin011121@gmail.com>
2023-02-25 21:30:16 +0800

    test fileio

---
+++ test/test_fileio.py
@@ -0,0 +1,15 @@
+from biliFile_with_steganography.utils.fileio import FileIO
+
+
+def test():
+    try:
+        for i in FileIO.read("test_data/test_fileio", 10):
+            ...
+        return 0
+    except:
+        raise
+
+
+if __name__ == "__main__":
+    # Test fileio
+    print(test())
