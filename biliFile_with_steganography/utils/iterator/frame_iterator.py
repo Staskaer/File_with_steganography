61d59d7ac87fc61076fe57a0c0a5f7f4f50096e8
Staskaer <liujiaxin011121@gmail.com>
2023-03-11 10:25:26 +0800

    frame iterator

--- biliFile_with_steganography/utils/iterator/frame_iterator.py
+++ biliFile_with_steganography/utils/iterator/frame_iterator.py
@@ -1,4 +1,3 @@
-from ..fileio import FileIO
 from typing import Tuple, Any
 import numpy as np
 import threading
@@ -40,10 +39,40 @@ class FrameReadIterator:
 class FrameWriteIterator:
     def __init__(self,
                  file: str,
+                 fourcc: Any,
                  fps: float,
-                 size: Tuple[int, int],
-                 fourcc: Any = cv2.VideoWriter_fourcc(*'mp4v')) -> None:
-        self.writer = cv2.VideoWriter(file, fourcc, fps, size)
+                 size: Tuple[int, int]) -> None:
+        self.writer = cv2.VideoWriter(file, fourcc, fps, size, False)
+        self.queue = []
+        self.writing = []
+        self.cond = threading.Condition()
+        self._stop = False
+        threading.Thread(target=self.do_flush).start()
+
+    def flush(self):
+        with self.cond:
+            self.writing = self.queue
+            self.queue = []
+            self.cond.notify_all()
+
+    def do_flush(self):
+        while (not self._stop):
+            with self.cond:
+                # 测试条件
+                while len(self.writing) == 0:
+                    self.cond.wait()
+                for img in self.writing:
+                    self.writer.write(img)
+                self.writing = []
 
     def write(self, frame: np.array):
-        self.writer.write(frame)
+        self.queue.append(frame)
+        if len(self.queue) > 600:
+            self.flush()
+
+    def stop(self):
+        self.flush()
+        self._stop = True
+
+    def __del__(self):
+        self.writer.release()
---
+++ test/test_frame_iterator.py
@@ -0,0 +1,12 @@
+from biliFile_with_steganography.utils.iterator import FrameReadIterator, FrameWriteIterator
+import cv2
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+
+f = FrameReadIterator("test/test_data/test1.mp4")
+w = FrameWriteIterator("test/test_data/test2.mp4",
+                       fourcc,
+                       f.fps,
+                       f.frame_size)
+for img in f:
+    w.write(img)
+w.stop()
